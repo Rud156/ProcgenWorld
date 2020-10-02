f0c12d9e74d80264cb9d0d1d6588ab1a3212f940
Rud156 <rudrangshu_1997@yahoo.com>
2020-10-01 19:04:19 -0600

    Testing Debug

---
+++ Source/ProcGenWorld/ProcGenWorldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProcGenWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCGENWORLD_API AProcGenWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

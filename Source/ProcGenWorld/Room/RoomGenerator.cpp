67ba06d302a20af265dc79f4358114ebe0ff9ef5
Rud156 <rudrangshu_1997@yahoo.com>
2020-10-03 14:11:35 -0600

    Room Generation Complete

--- Source/ProcGenWorld/Room/RoomGenerator.cpp
+++ Source/ProcGenWorld/Room/RoomGenerator.cpp
@@ -6,23 +6,190 @@
 // Sets default values
 ARoomGenerator::ARoomGenerator()
 {
-    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-    PrimaryActorTick.bCanEverTick = true;
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
+	_topRowDoorPosition = FVector::ZeroVector;
+	_bottomRowDoorPosition = FVector::ZeroVector;
+	_leftColumnDoorPosition = FVector::ZeroVector;
+	_rightColumnDoorPosition = FVector::ZeroVector;
 }
 
 // Called when the game starts or when spawned
 void ARoomGenerator::BeginPlay()
 {
-    Super::BeginPlay();
-    
-    
+	Super::BeginPlay();
+
+	FString fileName = "TestRoom.txt";
+
+	FString directory = FPaths::ProjectContentDir() + "/Data";
+	FString result;
+
+	FString filePath = directory + "/" + fileName;
+
+	IPlatformFile& fileManager = FPlatformFileManager::Get().GetPlatformFile();
+	if (fileManager.FileExists(*filePath)) {
+		FFileHelper::LoadFileToString(result, *filePath, FFileHelper::EHashOptions::None);
+
+		RenderRoomFromString(result, FVector(0, 560, 0));
+	}
+	else {
+		GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, "File Not Found");
+	}
 }
 
 // Called every frame
 void ARoomGenerator::Tick(float DeltaTime)
 {
-    Super::Tick(DeltaTime);
-    GEngine->AddOnScreenDebugMessage(-1, 0.2, FColor::Red, "Hello World: " + FString::SanitizeFloat(DeltaTime));
+	Super::Tick(DeltaTime);
 }
 
+void ARoomGenerator::RenderRoomFromString(FString roomString, FVector startPosition)
+{
+	_startPosition = startPosition;
+
+	FVector currentPosition = startPosition;
+	FRotator rotation90 = FRotator(0, 90, 0);
+
+	bool lastSpawnVertical = false;
+
+	int rowCount = 0;
+	int columnCount = 0;
+	bool rowCountCalculated = false;
+
+	bool topDoorPositionCalculated = false;
+	bool bottomDoorPositionCalculated = false;
+	bool leftDoorPositionCalculated = false;
+	bool rightDoorPositionCalculated = false;
+
+
+	int stringIndex = 0;
+	while (stringIndex < roomString.Len())
+	{
+		auto letter = roomString[stringIndex];
+
+		if (letter == '-')
+		{
+			GetWorld()->SpawnActor(WallPrefab, &currentPosition, &FRotator::ZeroRotator);
+
+			currentPosition.X += WallWidth;
+			lastSpawnVertical = false;
+		}
+		else if (letter == '|')
+		{
+			auto nextLetter = roomString[stringIndex + 1];
+
+			if (nextLetter != ' ') {
+				currentPosition.X += WallWidth - WallThickness * 1.25f;
+			}
+
+			GetWorld()->SpawnActor(WallPrefab, &currentPosition, &rotation90);
+
+			currentPosition.X += WallWidth;
+			lastSpawnVertical = true;
+		}
+		else if (letter == '\n')
+		{
+			auto nextLetter = roomString[stringIndex + 1];
+			if (nextLetter == '-') {
+				leftDoorPositionCalculated = true;
+				rightDoorPositionCalculated = true;
+			}
+
+			if (!lastSpawnVertical) {
+				currentPosition.Y += WallThickness;
+			}
+			else {
+				currentPosition.Y += WallWidth;
+			}
+
+			currentPosition.X = startPosition.X + WallThickness * 1.25f;
+
+			columnCount += 1;
+			rowCountCalculated = true;
+			topDoorPositionCalculated = true;
+		}
+		else if (letter == 'X') {
+			auto nextLetter = ' ';
+			if (stringIndex + 1 < roomString.Len()) {
+				nextLetter = roomString[stringIndex + 1];
+			}
+
+			if (!topDoorPositionCalculated) {
+				_topRowDoorPosition = currentPosition;
+			}
+			else if (!leftDoorPositionCalculated || !rightDoorPositionCalculated) {
+				if (nextLetter == ' ') {
+					_leftColumnDoorPosition = currentPosition;
+					leftDoorPositionCalculated = true;
+				}
+				else {
+					_rightColumnDoorPosition = currentPosition;
+					_rightColumnDoorPosition.X += WallWidth - WallThickness * 1.25f;
+					rightDoorPositionCalculated = true;
+				}
+			}
+			else if (!bottomDoorPositionCalculated) {
+				_bottomRowDoorPosition = currentPosition;
+				bottomDoorPositionCalculated = true;
+			}
+
+			currentPosition.X += WallWidth;
+		}
+		else if (letter == ' ')
+		{
+			currentPosition.X += WallWidth;
+		}
+
+		stringIndex += 1;
+		if (!rowCountCalculated) {
+			rowCount += 1;
+		}
+	}
+
+	columnCount += 1;
+	rowCount -= 1;
+
+	_rowCount = rowCount;
+	_columnCount = columnCount;
+
+	//GetWorld()->SpawnActor(TestDoorPrefab, &_topRowDoorPosition, &FRotator::ZeroRotator);
+	//GetWorld()->SpawnActor(TestDoorPrefab, &_leftColumnDoorPosition, &FRotator::ZeroRotator);
+	//GetWorld()->SpawnActor(TestDoorPrefab, &_rightColumnDoorPosition, &FRotator::ZeroRotator);
+	//GetWorld()->SpawnActor(TestDoorPrefab, &_bottomRowDoorPosition, &FRotator::ZeroRotator);
+}
+
+FVector ARoomGenerator::GetStartPosition()
+{
+	return _startPosition;
+}
+
+FVector ARoomGenerator::GetTopRowDoorPosition()
+{
+	return _topRowDoorPosition;
+}
+
+FVector ARoomGenerator::GetBottomRowDoorPosition()
+{
+	return _bottomRowDoorPosition;
+}
+
+FVector ARoomGenerator::GetLeftColumnDoorPosition()
+{
+	return _leftColumnDoorPosition;
+}
+
+FVector ARoomGenerator::GetRightColumnRowDoorPosition()
+{
+	return _rightColumnDoorPosition;
+}
+
+int ARoomGenerator::GetRowCount()
+{
+	return _rowCount;
+}
+
+int ARoomGenerator::GetColumnCount()
+{
+	return _columnCount;
+}
--- Source/ProcGenWorld/Room/RoomGenerator.h
+++ Source/ProcGenWorld/Room/RoomGenerator.h
@@ -10,17 +10,54 @@ UCLASS()
 class PROCGENWORLD_API ARoomGenerator : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+private:
+	int _rowCount;
+	int _columnCount;
+
+	FVector _startPosition;
+
+	FVector _topRowDoorPosition;
+	FVector _bottomRowDoorPosition;
+	FVector _leftColumnDoorPosition;
+	FVector _rightColumnDoorPosition;
+
+	void RenderRoomFromString(FString roomString, FVector startPosition);
+
+public:
+#pragma region Parameters
+
+	UPROPERTY(Category = Wall, EditAnywhere)
+		TSubclassOf<class AActor> WallPrefab;
+
+	UPROPERTY(Category = Wall, EditAnywhere)
+		TSubclassOf<class AActor> TestDoorPrefab;
+
+	UPROPERTY(Category = Wall, EditAnywhere)
+		float WallWidth;
+
+	UPROPERTY(Category = Wall, EditAnywhere)
+		float WallThickness;
+
+#pragma endregion
+
 	// Sets default values for this actor's properties
 	ARoomGenerator();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	FVector GetStartPosition();
+	FVector GetTopRowDoorPosition();
+	FVector GetBottomRowDoorPosition();
+	FVector GetLeftColumnDoorPosition();
+	FVector GetRightColumnRowDoorPosition();
+
+	int GetRowCount();
+	int GetColumnCount();
+
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 };
